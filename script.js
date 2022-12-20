2f4bba87b63aacb3e913559ef169cd9113d990f0
Alissa Jackson <alissa.j636@nebo.local>
2022-12-20 08:34:00 -0700

    Css and half of JS 101

---
+++ script.js
@@ -0,0 +1,32 @@
+console.log("Page Loaded script.js");
+
+var website = "https://codingforeverybody.com/";
+var studentName = "Arlo Jackson";
+console.log(studentName);
+
+// Const cannot be changed NEVER be changed, let can be rewritten
+
+let apples = 4;
+const height = "5'9\"";
+//   console.log("i have", apples, "apples.");
+//  console.log("I am", height);
+  
+// let cats = 3;
+/*const seniorCat = "lils";
+const kitten = "onyx"; 
+console.log ( "I have", cats ,"cats");*/
+
+console.warn(" Youngest is", kitten)
+
+const name =" Arlo Jackson";
+let profession = "Student";
+
+const str ="A String"; // String
+const num =4; // number
+const flt = 2.14; //float
+const bool = true; //or false
+const list =["item 1", 2, "three", 4.25, ]; // array=list
+const thing = null; //nothing in it
+const notDefined = undefined; // Currently no value.
+
+document.getElementById("demo").innerHTML = "Data Structures" + str + num + flt;
