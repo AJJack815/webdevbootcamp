957645556d4030e9ccd59bbffd7ac9f2622a7f47
AJ Jackson <alissajj815@gmail.com>
2023-01-06 10:05:48 -0700

    Friday Janurary 6th

--- script.js
+++ script.js
@@ -16,17 +16,39 @@ const height = "5'9\"";
 const kitten = "onyx"; 
 console.log ( "I have", cats ,"cats");*/
 
-console.warn(" Youngest is", kitten)
-
-const name =" Arlo Jackson";
-let profession = "Student";
+// console.warn(" Youngest is", kitten)
+//JavaScript Data types
+// let profession = "Student";
 
 const str ="A String"; // String
 const num =4; // number
 const flt = 2.14; //float
 const bool = true; //or false
-const list =["item 1", 2, "three", 4.25, ]; // array=list
-const thing = null; //nothing in it
-const notDefined = undefined; // Currently no value.
+const list =["item 1", 2, "three", 4.25, []]; // array=list
+const thing = null; //Nothing in it
+const notDefined = undefined; //Currently nothing in it, but can.
+const getAge = function() {return 17;} // This is  a function, allows us to use getAge a lot.
+const obj = {
+    'name': 'Arlo',
+    'profession': 'Student',
+}// Can be accessed with obj['name]
 
-document.getElementById("demo").innerHTML = "Data Structures" + str + num + flt;
+//Conditional Statements
+const age = 17;
+if( age >= 16) {
+    console.log("I can get a job");
+}
+if( age >= 21) {
+    console.log("I can drink.")
+} else { 
+    console.log(" Not old enough to drink")
+}
+const name= "Arlo";
+if(name == "Nathan"){
+    console.log("welcome, Nathan!")
+} else if (name == "Arlo"){
+    console.log("Hello Arlo!")
+}
+  
+
+document.getElementById("demo").innerHTML = "Data Structures " + str + num + flt;
